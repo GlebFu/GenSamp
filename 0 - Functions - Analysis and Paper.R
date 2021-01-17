c631d0d00cb98ff1e9acb5c98c3af0e67d301b42
glebfu <gleb.furman@gmail.com>
2021-01-16 19:08:08 -0600

    Updating results

--- 0 - Functions - Analysis and Paper.R
+++ 0 - Functions - Analysis and Paper.R
@@ -24,27 +24,38 @@ row_groups <- function(x, blank = F) {
 # PLOT FUNCTIONS
 ###################
 
-plot_smd <- function(data, yvar = "mSMD") {
+plot_smd <- function(data, yvar = "mSMD", l.pos = "bottom", g.title) {
   data %>% 
-    ggplot(aes(x = RR, y = get(yvar), color = Sampler, linetype = Sampler, group = Sampler)) +
+    ggplot(aes(x = RR, y = get(yvar), 
+               color = Sampler, 
+               linetype = Sampler, 
+               group = Sampler)) +
     geom_line(size = 1) +
     geom_hline(yintercept = .25, linetype = "dotted") +
     facet_wrap( ~ Variables, ncol = 3) +
     labs(y = lab.vars$smd,
-         x = lab.vars$rr) + 
+         x = lab.vars$rr,
+         title = g.title) + 
     scale_x_continuous(breaks = seq(10, 90, 20)) +
-    theme(legend.position = "bottom")
+    theme(legend.position = l.pos)
 }
 
-plot_smd2 <- function(data, yvar = "mSMD") {
+plot_smd2 <- function(data, yvar = "mSMD", l.pos = "right", g.title) {
   data %>% 
-    ggplot(aes(x = RR, y = get(yvar), color = Sampler, linetype = Sampler, group = Sampler)) +
+    ggplot(aes(x = RR, y = get(yvar), 
+               color = Sample, 
+               linetype = Cluster, 
+               group = Sampler)) +
     geom_line(size = 1) +
-    geom_hline(yintercept = .25, linetype = "dotted") +
+    geom_hline(yintercept = c(.25), linetype = "dotted") +
+    geom_hline(yintercept = 0, linetype = "solid") +
     labs(y = lab.vars$smd,
-         x = lab.vars$rr) + 
-    scale_x_continuous(breaks = seq(10, 90, 20)) +
-    theme(legend.position = "right")
+         x = lab.vars$rr,
+         title = g.title) + 
+    scale_x_continuous(breaks = seq(10, 90, 20),
+                       limits = c(10, 100)) +
+    theme(legend.position = l.pos) +
+    facet_wrap(~SB, nrow = 1) 
 }
 
 apa_style_plot <- function(x) {
