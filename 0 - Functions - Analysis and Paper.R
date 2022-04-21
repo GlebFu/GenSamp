fc89f6ca7da23416f99ea431ad8ccfa5b9770c48
glebfu <gleb.furman@gmail.com>
2022-04-21 17:37:31 -0500

    Finished draft

--- 0 - Functions - Analysis and Paper.R
+++ 0 - Functions - Analysis and Paper.R
@@ -57,6 +57,42 @@ plot_smd <- function(data, l.pos = "right", g.title = waiver(), x.title = waiver
     facet_wrap(~SF_fac, nrow = 1) 
 }
 
+plot_smd2 <- function(data, l.pos = "right", g.title = waiver(), x.title = waiver(), y.title = waiver()) {
+  data %>% 
+    ggplot(aes(x = RR, y = Value,
+               alpha = alpha.group)) %>%
+    common_line() %>%
+    common_point() +
+    geom_hline(yintercept = c(.25), linetype = "dotted") +
+    geom_hline(yintercept = 0, linetype = "solid") +
+    labs(y = y.title,
+         x = x.title,
+         title = g.title) + 
+    scale_x_continuous(breaks = seq(10, 90, 20),
+                       limits = c(10, 100)) +
+    scale_alpha_continuous(guide = "none", range = c(.5, 1)) +
+    theme(legend.position = l.pos) +
+    facet_wrap(~SF_fac + Group, nrow = 1) 
+}
+
+plot_smd3 <- function(data, l.pos = "right", g.title = waiver(), x.title = waiver(), y.title = waiver()) {
+  data %>% 
+    ggplot(aes(x = RR, y = Value,
+               alpha = alpha.group)) %>%
+    common_line() %>%
+    common_point() +
+    geom_hline(yintercept = c(.25), linetype = "dotted") +
+    geom_hline(yintercept = 0, linetype = "solid") +
+    labs(y = y.title,
+         x = x.title,
+         title = g.title) + 
+    scale_x_continuous(breaks = seq(10, 90, 20),
+                       limits = c(10, 100)) +
+    scale_alpha_continuous(guide = "none", range = c(.5, 1)) +
+    theme(legend.position = l.pos) +
+    facet_wrap(~SF_fac + Group, nrow = 1) 
+}
+
 apa_style_plot <- function(x) {
   x +
     theme_apa() + 
@@ -66,3 +102,17 @@ apa_style_plot <- function(x) {
     theme(legend.position = "right") +
     scale_color_brewer(type = "qual", palette = 6)
 }
+
+smd.limits <- 
+  function(x)  {
+    newlim <- c(-.1, ceiling(max(x) * 10) / 10)
+    newlim
+  }
+
+smd.break <- 
+  function(x) {
+    x.max <- max(smd.limits(x))
+    x.break <- ceiling(x.max * 2) / 10
+    newbreak <- seq(0, x.max, x.break)
+    newbreak
+  }
